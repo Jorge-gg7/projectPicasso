f3bcc30888442357827000e72bc1dec09ce8b28c
Brandon <ekhai_chew@yahoo.com>
2023-03-02 20:23:27 +0000

    Finished csv database and cleaning data

--- data_cleansing.py
+++ data_cleansing.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 pd.set_option('display.max_columns', None)
 
+######################################        First Dataset        #####################################################
+
 ### Getting the raw data by web scraping
 url_link1 = 'https://fbref.com/en/squads/19538871/2022-2023/all_comps/Manchester-United-Stats-All-Competitions'
 df_unclean_2022_2023 = pd.read_html(url_link1, header=1)
@@ -68,11 +70,39 @@ df_2022_2023 = df_2022_2023[~df_2022_2023["Player"].isin(new_players)].reset_ind
 ### competitions, I decided to delete the other goalkeepers.
 df_gk_2022_2023 = df_unclean_2022_2023[11]
 df_gk_2022_2023.dropna(subset=['FK'], inplace=True)
+renaming2 = {'Att': 'Att_Launch', 'Att.1': 'Att_Pass', 'Att.2': 'Att_GKicks', 'AvgLen': 'AvgLen_Pass', 'AvgLen.1': 'AvgLen_GKicks',
+             'Launch%': 'Launch%_Pass', 'Launch%.1': 'Launch%_GKicks'}
+df_gk_2022_2023.rename(columns=renaming2, inplace=True)
 
-
+df_2022_2023.to_csv('./data/data_2022_2023.csv', index=True)
+df_gk_2022_2023.to_csv('./data/df_2022_2023_gk.csv', index=True)
+df_2022_2023_new.to_csv('./data/df_2022_2023_new_signings.csv', index=True)
 
 ######################################        Second Dataset        ####################################################
 url_link2 = 'https://fbref.com/en/squads/19538871/2021-2022/all_comps/Manchester-United-Stats-All-Competitions'
 df_unclean_2021_2022 = pd.read_html(url_link2, header=1)
 
-print()
+df_2021_2022 = df_unclean_2021_2022[16]
+df_2021_2022.drop('Matches', axis=1, inplace=True)
+
+for i in range(21, 52, 5):
+    colu = ['Player', 'Nation', 'Pos', '90s', 'Matches', 'Age']
+    dff = df_unclean_2021_2022[i]
+    dff.drop(columns=colu, axis=1, inplace=True)
+    df_2021_2022 = df_2021_2022.join(dff, how="outer", rsuffix='_%a' % i)
+
+df_2021_2022 = df_2021_2022[df_2021_2022['Player'].notna()]
+df_2021_2022.fillna(0, inplace=True)
+df_2021_2022.rename(columns=renaming, inplace=True)
+df_2021_2022.rename(columns=renaming1, inplace=True)
+
+df_gk_2021_2022 = df_unclean_2021_2022[11]
+df_gk_2021_2022.drop(df_gk_2021_2022[df_gk_2021_2022['90s']<5].index, inplace=True)
+df_gk_2021_2022.drop('Matches', axis=1, inplace=True)
+df_gk_2021_2022.rename(columns=renaming2, inplace=True)
+
+df_2021_2022.to_csv('./data/data_2021_2022.csv', index=True)
+df_gk_2021_2022.to_csv('./data/df_2021_2022_gk.csv', index=True)
+
+
+
